2f1a9f4183437096e180377981ae153f05852f67
Haimrich <haimrich@live.it>
2021-10-26 09:29:56 +0000

    improved accelergy cache performance

--- src/accelergy.hpp
+++ src/accelergy.hpp
@@ -1,11 +1,14 @@
 #ifndef MEDEA_ACCELERGY_H_
 #define MEDEA_ACCELERGY_H_
 
+#include <string>
 #include <vector>
 #include <map>
+#include <mutex>
+
+#include "yaml-cpp/yaml.h"
 
 #include "compound-config/compound-config.hpp"
-#include <yaml-cpp/yaml.h>
 
 namespace medea
 {
@@ -13,17 +16,81 @@ namespace medea
   {
   protected:
     struct Entry {
-      YAML::Node energy;
-      YAML::Node area;
+      std::string energy;
+      std::string area;
     };
 
     std::mutex cache_mutex_;
     std::map<std::string, std::map<size_t, Entry>> cache_;
-    std::vector<Entry> invariant_nodes_;
-    bool invariant_nodes_inizialized_;
+
+    Entry invariant_nodes_;
+    std::once_flag invariant_nodes_inizialized_;
 
     std::string fast_accelergy_path_;
     config::CompoundConfig *config_;
+
+  public:
+  
+    struct RT {
+      config::CompoundConfigNode energy;
+      config::CompoundConfigNode area;
+    };
+
+
+    Accelergy(config::CompoundConfig *config)
+    {
+      config_ = config;
+      fast_accelergy_path_ = BUILD_BASE_DIR;
+      fast_accelergy_path_ += "/../../scripts/fast_accelergy.py";
+      config->getRoot().lookup("medea").lookupValue("fast-accelergy-path", fast_accelergy_path_);
+    }
+
+
+    RT GetReferenceTables(std::map<std::string, uint64_t> &updates, std::string out_prefix)
+    {
+      YAML::Node ert_node = YAML::Node();
+      YAML::Node art_node = YAML::Node();
+
+      if ( ! FindInCache(updates, ert_node, art_node) ) {
+
+        std::vector<std::string> input_files = config_->inFiles;
+
+        std::string cmd = "python " + fast_accelergy_path_;
+
+        for (auto input_file : input_files)
+          cmd += " " + input_file;
+
+        cmd += " --oprefix " + out_prefix + ". --updates ";
+
+        for (std::map<std::string, uint64_t>::iterator it = updates.begin(); it != updates.end(); it++)
+          cmd += " " + it->first + "," + std::to_string(it->second);
+
+        std::string fast_accelergy_out = Run(cmd.c_str());
+        if (fast_accelergy_out.length() == 0)
+        {
+          std::cout << "Failed to run Accelergy. Did you install Accelergy or specify ACCELERGYPATH correctly? Or check accelergy.log to see what went wrong" << std::endl;
+          exit(0);
+        }
+
+        YAML::Node acc_out = YAML::Load(fast_accelergy_out);
+        ert_node = acc_out["ERT"];
+        art_node = acc_out["ART"];
+
+        std::call_once ( invariant_nodes_inizialized_, [&]{ 
+          InizializeInvariantNodes(ert_node, art_node, updates); 
+        } );          
+
+        UpdateCache(ert_node, art_node, updates);
+      }
+      
+      auto art_config = config::CompoundConfigNode(nullptr, art_node, config_);
+      auto ert_config = config::CompoundConfigNode(nullptr, ert_node, config_);
+
+      return (RT) {.energy = ert_config, .area = art_config};
+    }
+  
+
+  protected:
 
     std::string Run(const char *cmd)
     {
@@ -49,141 +116,81 @@ namespace medea
       return result;
     }
 
-  public:
-    struct RT {
-      config::CompoundConfigNode energy;
-      config::CompoundConfigNode area;
-    };
 
-    Accelergy(config::CompoundConfig *config) :
-      invariant_nodes_inizialized_(false)
-    {
-      config_ = config;
-      fast_accelergy_path_ = BUILD_BASE_DIR;
-      fast_accelergy_path_ += "/../../scripts/fast_accelergy.py";
-      config->getRoot().lookup("medea").lookupValue("fast-accelergy-path", fast_accelergy_path_);
-    }
+    void InizializeInvariantNodes(YAML::Node &ert_node, YAML::Node &art_node, std::map<std::string, uint64_t> &updates) {
+      YAML::Emitter ytmpe, ytmpa;
+      ytmpe << YAML::BeginSeq;
+      ytmpa << YAML::BeginSeq;
 
-
-    RT GetReferenceTables(std::map<std::string, uint64_t> &updates, std::string out_prefix)
-    {
-      YAML::Node ynode = YAML::Node();
-      if (!FindInCache(updates, ynode)) {
-
-        std::vector<std::string> input_files = config_->inFiles;
-
-        std::string cmd = "python " + fast_accelergy_path_;
-
-        for (auto input_file : input_files)
-          cmd += " " + input_file;
-
-        cmd += " --oprefix " + out_prefix + ". --updates ";
-
-        for (std::map<std::string, uint64_t>::iterator it = updates.begin(); it != updates.end(); it++)
-          cmd += " " + it->first + "," + std::to_string(it->second);
-
-        std::string fast_accelergy_out = Run(cmd.c_str());
-        if (fast_accelergy_out.length() == 0)
-        {
-          std::cout << "Failed to run Accelergy. Did you install Accelergy or specify ACCELERGYPATH correctly? Or check accelergy.log to see what went wrong" << std::endl;
-          exit(0);
-        }
-        ynode = YAML::Load(fast_accelergy_out);
-        if (!invariant_nodes_inizialized_) InizializeInvariantNodes(ynode, updates);
-
-        UpdateCache(ynode, updates);
-      }
-      
-      auto art_node = config::CompoundConfigNode(nullptr, ynode["ART"], config_);
-      auto ert_node = config::CompoundConfigNode(nullptr, ynode["ERT"], config_);
-
-      return (RT) {.energy = ert_node, .area = art_node};
-    }
-
-
-    void InizializeInvariantNodes(YAML::Node &acc_out, std::map<std::string, uint64_t> &updates) {
-      std::unique_lock<std::mutex> lock(cache_mutex_);
-      if (invariant_nodes_inizialized_) return;
-
-      for (std::size_t i = 0; i < acc_out["ART"]["tables"].size(); i++) {
-        auto name = acc_out["ART"]["tables"][i]["name"].as<std::string>();
+      for (std::size_t i = 0; i < art_node["tables"].size(); i++) {
+        auto name = art_node["tables"][i]["name"].as<std::string>();
         name = name.substr(name.find_last_of(".") + 1);
 
-        if (updates.find(name) == updates.end())
-          invariant_nodes_.push_back((Entry){.energy = acc_out["ERT"]["tables"][i], .area = acc_out["ART"]["tables"][i]});
+        if (updates.find(name) == updates.end()) {
+          ytmpe << ert_node["tables"][i];
+          ytmpa << art_node["tables"][i];
+        }
       }
-      invariant_nodes_inizialized_ = true;
+
+      ytmpe << YAML::EndSeq;
+      ytmpa << YAML::EndSeq;
+
+      invariant_nodes_.energy = ytmpe.c_str();
+      invariant_nodes_.area = ytmpa.c_str();
     }
 
 
-    bool FindInCache(std::map<std::string, uint64_t> &updates, YAML::Node &root) {
-      YAML::Node art, ert;
+    bool FindInCache(std::map<std::string, uint64_t> &updates, YAML::Node &ert_node, YAML::Node &art_node) {
+      std::string art, ert;
+      art = ert = "version: 0.3\ntables:\n";
 
-      cache_mutex_.lock();
       try {
+        std::lock_guard<std::mutex> lock(cache_mutex_);
         for (auto it = updates.begin(); it != updates.end(); it++)
         {
-          Entry &entry = cache_.at(it->first).at(it->second);
-          art["tables"].push_back(entry.area);
-          ert["tables"].push_back(entry.energy);
+          Entry &entry = cache_.at(it->first).at(it->second);  
+          art += entry.area + "\n";
+          ert += entry.energy + "\n";
         }
       }
       catch (const std::out_of_range& oor) {
-        cache_mutex_.unlock();
         return false;
       }
-      cache_mutex_.unlock();
 
-      for (auto &e : invariant_nodes_) {
-        art["tables"].push_back(e.area);
-        ert["tables"].push_back(e.energy);
-      }
-
-      art["version"] = 0.3;
-      ert["version"] = 0.3;
-
-      root["ART"] = art;
-      root["ERT"] = ert;
+      art_node = YAML::Load(art + invariant_nodes_.area);
+      ert_node = YAML::Load(ert + invariant_nodes_.energy);
 
       //std::cout << "[INFO] Accelergy Cache Hit! " << std::endl;
       return true;
     }
 
 
-    void UpdateCache(YAML::Node &acc_out, std::map<std::string, uint64_t> &updates) {
+    void UpdateCache(YAML::Node &ert_node, YAML::Node &art_node, std::map<std::string, uint64_t> &updates) {
       std::unique_lock<std::mutex> lock(cache_mutex_);
 
-      for (std::size_t i = 0; i < acc_out["ART"]["tables"].size(); i++) {
-        auto name = acc_out["ART"]["tables"][i]["name"].as<std::string>();
+      for (std::size_t i = 0; i < art_node["tables"].size(); i++) {
+        auto name = art_node["tables"][i]["name"].as<std::string>();
         name = name.substr(name.find_last_of(".") + 1);
+        
 
         auto size = updates.find(name);
-        if (size != updates.end())
-          cache_[name][size->second] = (Entry){.energy = acc_out["ERT"]["tables"][i], .area = acc_out["ART"]["tables"][i]};
+        if (size != updates.end()) {
+          YAML::Emitter ytmpe, ytmpa;
+
+          ytmpe << YAML::BeginSeq << ert_node["tables"][i] << YAML::EndSeq;
+          ytmpa << YAML::BeginSeq << art_node["tables"][i] << YAML::EndSeq;
+
+          cache_[name][size->second] = (Entry) {
+            .energy = ytmpe.c_str(),
+            .area = ytmpa.c_str()
+          };
+        }
+        
       }
     }
 
   };
 
 }
-
-/* maybe useful for caching
-
-YAML::Node node = root_node;
-while (node["subtree"]) {
-node = node["subtree"][0];
-auto local_node = node["local"];
-
-if (local_node) 
-    for (auto&& buffer : local_node) 
-    if (buffer["name"]) {
-        auto buffer_name = buffer["name"].as<std::string>();
-        if (buffer_name.compare(0, storage_name.size(), storage_name) == 0 && buffer["attributes"])
-        if (buffer["attributes"]["depth"])
-            buffer["attributes"]["depth"] = 
-    }
-    
-}
-*/
 
 #endif // MEDEA_ACCELERGY_H_
